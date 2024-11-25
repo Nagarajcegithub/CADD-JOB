d15fd7fde7d6cc1ecbe51bd1d931c5f13f24dce4
Nagarajcegithub <nagaraj.ce18@gmail.com>
2024-11-25 10:56:07 +0530

    Create test.sv

---
+++ orange/b/test.sv
@@ -0,0 +1,12 @@
+module job_q_2.2_tb;
+reg A,B,C,D,E;
+wire Y;
+job_q_2.2  dut(A,B,C,D,E,Y);
+initial 
+begin
+#10;A=0;B=0;C=0;D=0;E=0;
+#10;A=0;B=0;C=0;D=0;E=1;
+#10;A=0;B=0;C=0;D=1;E=0;
+#10;A=0;B=0;C=0;D=1;E=1;
+end
+endmodule
